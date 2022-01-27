7214efa15db95df859ebeb0c0853215f1fcb35c8
Prescilla Lecurieux <prescilla.lecurieux@energisme.com>
2022-01-27 18:46:32 +0100

    add Uber BaseWeb

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import '../styles/globals.css'
+import App from 'next/app'
+import {styletron} from '../styletron';
+import {Provider as StyletronProvider} from 'styletron-react';
+import { BaseProvider, LightTheme } from 'baseui'
+
+export default class MyApp extends App {
+ render() {
+  const {Component, pageProps} = this.props;
+  return (
+   <StyletronProvider value={styletron}>
+    <BaseProvider theme={LightTheme}>
+     <Component {...pageProps} />
+    </BaseProvider>
+   </StyletronProvider>
+  );
+ }
+}

41dc90414525cbfc767ea8769610e06c250d06ac
Prescilla Lecurieux <prescilla.lecurieux@energisme.com>
2022-04-19 10:43:17 +0200

    add menu items

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,7 +1,10 @@
 import {Drawer} from 'baseui/drawer'
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import {Button, KIND} from 'baseui/button'
 import {Checkbox, STYLE_TYPE} from 'baseui/checkbox'
+import {Navigation} from 'baseui/side-navigation'
+import {useRouter} from 'next/router'
+import {menuItems} from '../../utils/data'
 
 type MenuProps = {
 	isOpen: boolean
@@ -11,9 +14,36 @@ type MenuProps = {
 }
 
 const Menu = ({isOpen, isDark, onClose, setTheme}: MenuProps) => {
+	const router = useRouter()
+	const [location, setLocation] = useState(router.pathname)
 	return (
 		<Fragment>
 			<Drawer onClose={onClose} isOpen={isOpen} anchor={'right'}>
+				<Navigation
+					items={menuItems}
+					activeItemId={location}
+					onChange={({item}) => setLocation(item.itemId)}
+					overrides={{
+						NavItem: {
+							style: ({$active, $theme}) => {
+								if (!$active)
+									return {
+										':hover': {
+											color: $theme.colors.warning400,
+										},
+									}
+								return {
+									backgroundColor: 'transparent',
+									borderLeftColor: $theme.colors.warning400,
+									color: $theme.colors.warning400,
+									':hover': {
+										color: $theme.colors.warning400,
+									},
+								}
+							},
+						},
+					}}
+				/>
 				<Button onClick={setTheme} kind={KIND.tertiary}>
 					<Checkbox checked={isDark} checkmarkType={STYLE_TYPE.toggle_round} />
 					Dark mode
